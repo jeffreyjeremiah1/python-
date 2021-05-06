c154c96fce592d752e06be827c269dc7f24c3d2b
jeffreyjeremiah1 <83784485+jeffreyjeremiah1@users.noreply.github.com>
2021-05-06 22:30:04 +0300

    Add files via upload

---
+++ menu.py
@@ -0,0 +1,257 @@
+foods = {
+    "almond pudding": {
+        "contents": {
+            "calories": 500,
+            "fats": 22.4,
+            "protein": 19.6,
+        }
+    },
+    "gozleme": {
+        "contents": {
+            "calories": 500,
+            "fats": 22.4,
+            "protein": 19.6,
+        }
+    },
+    "hummus": {
+        "contents": {
+            "calories": 33,
+            "fats": 2.1,
+            "protein": 1.2,
+        }
+    },
+    "iskender kebab": {
+        "contents": {
+            "calories": 816,
+            "fats": 31,
+            "protein": 60,
+        }
+    },
+    "karnabahar yemegi": {
+        "contents": {
+            "calories": 112,
+            "fats": 6.7,
+            "protein": 3.5,
+        }
+    },
+    "karniyarik": {
+        "contents": {
+            "calories": 375,
+            "fats": 13,
+            "protein": 13,
+        }
+    },
+    "kunefe": {
+        "contents": {
+            "calories": 497,
+            "fats": 25.5,
+            "protein": 11.8,
+        }
+    },
+    "lahmacun": {
+        "contents": {
+            "calories": 322,
+            "fats": 4.5,
+            "protein": 14.3,
+        }
+    },
+     "manti": {
+        "contents": {
+            "calories": 170,
+            "fats": 7.1,
+            "protein": 11.1,
+        }
+    },
+    "meatballs": {
+        "contents": {
+            "calories": 371,
+            "fats": 1.5,
+            "protein": 10.8,
+        }
+    },
+    "menemen": {
+        "contents": {
+            "calories": 181,
+            "fats": 12.3,
+            "protein": 8.6,
+        }
+    },
+    "mensaf": {
+        "contents": {
+            "calories": 1394,
+            "fats": 79,
+            "protein": 73.6,
+        }
+    },
+    "mezze": {
+        "contents": {
+            "calories": 560,
+            "fats": 44,
+            "protein": 15,
+        }
+    },
+    "pide": {
+        "contents": {
+            "calories": 280,
+            "fats": 3.9,
+            "protein": 9.2,
+        }
+    },
+    "pizza": {
+        "contents": {
+            "calories": 319,
+            "fats": 19.5,
+            "protein": 35.9,
+        }
+    },
+    "pilav": {
+        "contents": {
+            "calories": 307,
+            "fats": 10.9,
+            "protein": 4.2,
+        }
+    },
+    "rice pudding": {
+        "contents": {
+            "calories": 220,
+            "fats": 5.5,
+            "protein": 6.9,
+        }
+    },
+     "sigara borek": {
+        "contents": {
+            "calories": 79,
+            "fats": 6.2,
+            "protein": 2.6,
+        }
+    },
+    "spinach and cheese gozleme": {
+        "contents": {
+            "calories": 650,
+            "fats": 30.1,
+            "protein": 18.2,
+        }
+    },
+    "tavuk baget": {
+        "contents": {
+            "calories": 238,
+            "fats": 17.7,
+            "protein": 18.17,
+        }
+    },
+    "tavuk burger": {
+        "contents": {
+            "calories": 484,
+            "fats": 27.74,
+            "protein": 35.21,
+        }
+    },
+    "tavuk corbasi": {
+        "contents": {
+            "calories": 132,
+            "fats": 4.71,
+            "protein": 9.53,
+        }
+    },
+    "tavuk dolma": {
+        "contents": {
+            "calories": 489,
+            "fats": 22.09,
+            "protein": 29.8,
+        }
+    },
+    "tavuk doner": {
+        "contents": {
+            "calories": 149,
+            "fats": 19.68,
+            "protein": 20.64,
+        }
+    },
+    "tavuk kanadi": {
+        "contents": {
+            "calories": 252,
+            "fats": 7.08,
+            "protein": 43.94,
+        }
+    },
+    "tavuk kavurma": {
+        "contents": {
+            "calories": 333,
+            "fats": 13.03,
+            "protein": 46.08,
+        }
+    },
+    "tavuk kebap": {
+        "contents": {
+            "calories": 345,
+            "fats": 2.4,
+            "protein": 50.78,
+        }
+    },
+    "tavuk sarma": {
+        "contents": {
+            "calories": 500,
+            "fats": 16.79,
+            "protein": 44.24,
+        }
+    },
+    "tavuk sis": {
+        "contents": {
+            "calories": 208,
+            "fats": 7.9,
+            "protein": 30.52,
+        }
+    },
+    "tavuk sote": {
+        "contents": {
+            "calories": 288,
+            "fats": 15.2,
+            "protein": 28.46,
+        }
+    },
+    "tavuk tantuni": {
+        "contents": {
+            "calories": 415,
+            "fats": 15.81,
+            "protein": 26.14,
+        }
+    },
+    "taze fasulye": {
+        "contents": {
+            "calories": 327.5,
+            "fats": 0.6,
+            "protein": 4.6,
+        }
+    },
+    "zeytinyagli barbunya": {
+        "contents": {
+            "calories": 356,
+            "fats": 14.1,
+            "protein": 16,
+        }
+    },
+    "zeytinyagli enginar": {
+        "contents": {
+            "calories": 148,
+            "fats": 5,
+            "protein": 5,
+        }
+    },
+    "zeytinyagli pirasa": {
+        "contents": {
+            "calories": 114,
+            "fats": 5.5,
+            "protein": 2.7,
+        }
+    },
+
+}
+
+
+# choice = input("Enter any food name: ")
+# select = choice.lower()
+#
+#fhgfjhg/
+# print(foods[select]["contents"]["calories"])
+# print(foods[select]["contents"]["fats"])
+# print(foods[select]["contents"]["protein"])
